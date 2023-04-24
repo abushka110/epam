00e4a7c47316c09e702167e63935420ef4825bf8
abushka110 <qyjodixilu@gmail.com>
2023-04-24 10:46:54 +0300

    first dot net task

---
+++ dot_net/task_1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Write notebook price:");
+        int notebookPrice = int.Parse(Console.ReadLine());
+        Console.WriteLine("How many notebooks to buy: ");
+        int notebookCount = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Write pen price: ");
+        int penPrice = int.Parse(Console.ReadLine());
+        Console.WriteLine("How many pens to buy: ");
+        int penCount = int.Parse(Console.ReadLine());
+
+        int copyBookSumPrice = notebookPrice * notebookCount;
+        int penSumPrice = penPrice * penCount;
+        int fullPrice = copyBookSumPrice + penSumPrice;
+
+        Console.WriteLine($"Total price of {notebookCount} notebooks and {penCount} pens is: {fullPrice}");
+    }
+}
