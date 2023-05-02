f1daa95dec907ace201e05c9d31b05dff3011396
abushka110 <qyjodixilu@gmail.com>
2023-05-03 00:21:20 +0300

    better solution for 1st task

--- dot_net/task_1/ConsoleApp1/Program.cs
+++ dot_net/task_1/ConsoleApp1/Program.cs
@@ -5,18 +5,20 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("Write notebook price:");
-        int notebookPrice = int.Parse(Console.ReadLine());
-        Console.WriteLine("How many notebooks to buy: ");
-        int notebookCount = int.Parse(Console.ReadLine());
-
+        double notebookPrice = double.Parse(Console.ReadLine());
+        
         Console.WriteLine("Write pen price: ");
-        int penPrice = int.Parse(Console.ReadLine());
-        Console.WriteLine("How many pens to buy: ");
-        int penCount = int.Parse(Console.ReadLine());
+        double penPrice = double.Parse(Console.ReadLine());
 
-        int copyBookSumPrice = notebookPrice * notebookCount;
-        int penSumPrice = penPrice * penCount;
-        int fullPrice = copyBookSumPrice + penSumPrice;
+        Console.WriteLine("How many notebooks to buy: ");
+        int notebookCount = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("How many pens to buy: ");
+        int penCount = Convert.ToInt32(Console.ReadLine());
+
+        double copyBookSumPrice = notebookPrice * notebookCount;
+        double penSumPrice = penPrice * penCount;
+        double fullPrice = copyBookSumPrice + penSumPrice;
 
         Console.WriteLine($"Total price of {notebookCount} notebooks and {penCount} pens is: {fullPrice}");
     }
