58ece795d98c82dd10c41fab4317e8e2c38ad2c7
abushka110 <qyjodixilu@gmail.com>
2023-05-03 03:07:19 +0300

    second task, first part

---
+++ dot_net/task_2/task_2-1/task_2-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Скільки кошутє зошит? ");
+        double notebookPrice = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Скільки кошутє ручка? ");
+        double penPrice = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Скільки зошитів потрібно? ");
+        int notebookCount = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Скільки ручок потрібно? ");
+        int penCount = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Скільки грошей маєте?");
+        double howMuchMoney = double.Parse(Console.ReadLine());
+
+        double fullPrice = notebookPrice * notebookCount + penPrice * penCount;
+
+        if (howMuchMoney > fullPrice)
+        {
+            Console.WriteLine($"Якщо купити {notebookCount} зошитів і {penCount} ручок в вас залишиться {howMuchMoney % fullPrice}грн");
+        }
+        else
+        {
+            Console.WriteLine($"Вам не вистачить грошей на {notebookCount} зошитів і {penCount} ручок");
+        }
+    }
+}
