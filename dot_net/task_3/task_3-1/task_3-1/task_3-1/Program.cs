5115f11a5483ad58005e881129557c14e6daa31c
abushka110 <qyjodixilu@gmail.com>
2023-05-09 23:37:06 +0300

    task 3 part 1

---
+++ dot_net/task_3/task_3-1/task_3-1/task_3-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number A: ");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter a number B: ");
+        int B = int.Parse(Console.ReadLine());
+
+        for (int i = A; i >= B; i--)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadKey();
+    }
+}
