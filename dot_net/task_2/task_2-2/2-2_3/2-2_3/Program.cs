274b6501ff1b910441e280e638ab0e24a50b81b7
abushka110 <qyjodixilu@gmail.com>
2023-05-09 14:12:04 +0300

    task 2 part 2 third solution

---
+++ dot_net/task_2/task_2-2/2-2_3/2-2_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number, firstNumber, secondNumber, thirdNumber, temp;
+
+        Console.WriteLine("Enter a three-digit number:");
+        number = Convert.ToInt32(Console.ReadLine());
+        firstNumber = number / 100;
+        secondNumber = number / 10 % 10;
+        thirdNumber = number % 10;
+
+        if (firstNumber < secondNumber)
+        {
+            temp = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temp;
+        }
+
+        if (secondNumber < thirdNumber)
+        {
+            temp = secondNumber;
+            secondNumber = thirdNumber;
+            thirdNumber = temp;
+        }
+
+        if (firstNumber < secondNumber)
+        {
+            temp = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temp;
+        }
+
+        number = firstNumber * 100 + secondNumber * 10 + thirdNumber;
+        Console.WriteLine($"Maximum number is: {number}");
+    }
+}
