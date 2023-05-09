2e1b1ae468c2cf2bc1ac35bb7aa498fd1b3c2d2c
abushka110 <qyjodixilu@gmail.com>
2023-05-09 13:57:17 +0300

    task 2 part 2 first solution

---
+++ dot_net/task_2/task_2-2/2-2_1/2-2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Write number: ");
+        string number = Console.ReadLine();
+
+        Console.WriteLine($"Highest possible number with reordered digits: {string.Concat(number.OrderByDescending(c => c))}");
+    }
+}
