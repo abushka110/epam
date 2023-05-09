11f0b0d351cf5158ecbe233698fd9c2066cf0dee
abushka110 <qyjodixilu@gmail.com>
2023-05-09 13:57:59 +0300

    task 2 part 2 second solution

---
+++ dot_net/task_2/task_2-2/2-2_2/2-2_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class program
+{
+    public static void Main()
+    {
+        int num, n1, n2, n3;
+
+        num = Convert.ToInt32(Console.ReadLine());
+        n1 = num / 100;
+        n2 = num % 100 / 10;
+        n3 = num % 10;
+
+        if (n1 >= n2 & n1 >= n3)
+            if (n2 > n3)
+                Console.WriteLine(num);
+            else
+                Console.WriteLine(n1 * 100 + n3 * 10 + n2);
+        else if (n2 >= n1 & n2 >= n3)
+            if (n1 > n3)
+                Console.WriteLine(n2 * 100 + n1 * 10 + n3);
+            else
+                Console.WriteLine(n2 * 100 + n3 + 10 + n1);
+        else if (n2 > n1)
+            Console.WriteLine(n3 * 100 + n2 * 10 + n1);
+        else
+            Console.WriteLine(n3 * 100 + n1 * 10 + n2);
+    }
+}
