ad547afd427722dbdc67f8ec88b17db64f35ffa8
abushka110 <qyjodixilu@gmail.com>
2023-05-22 00:33:52 +0300

    task 5 part 1

---
+++ dot_net/task_5/task_5-1/task_5-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the first integer: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second integer: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        double average = CalculateAverage(number1, number2);
+        Console.WriteLine("Arithmetic mean value: " + average);
+    }
+
+    public static double CalculateAverage(int num1, int num2)
+    {
+        return (num1 + num2) / 2.0;
+    }
+}
