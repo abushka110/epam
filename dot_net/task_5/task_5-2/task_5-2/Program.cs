185e7b24518855a50bf6864689fd380e4d6ab101
abushka110 <qyjodixilu@gmail.com>
2023-05-22 00:37:53 +0300

    task 5 part 2

---
+++ dot_net/task_5/task_5-2/task_5-2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] array = { 5, 10, 15, 20, 25 };
+
+        int max = array.Max();
+        Console.WriteLine("Maximum value: " + max);
+    }
+}
