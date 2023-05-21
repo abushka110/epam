2ef99c259830774a26d5cfe1a78852a716677333
abushka110 <qyjodixilu@gmail.com>
2023-05-22 00:06:29 +0300

    task 4 part 1

---
+++ dot_net/task_4/task_4-1/task_4-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { -2, 3, 0, 7, -1, 9, 10, -4, 6, -5 };
+
+        int sumOfPositiveNumbers = 0;
+        int countOfEvenNumbers = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int number = numbers[i];
+
+            if (number > 0)
+            {
+                sumOfPositiveNumbers += number;
+            }
+            if (number % 2 == 0)
+            {
+                countOfEvenNumbers++;
+            }
+        }
+
+        Console.WriteLine("The sum of positive elements: " + sumOfPositiveNumbers);
+        Console.WriteLine("The number of even elements: " + countOfEvenNumbers);
+
+        Console.ReadLine();
+    }
+}
