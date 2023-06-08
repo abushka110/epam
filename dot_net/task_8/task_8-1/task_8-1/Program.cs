4066593e43bd9d53ecbb7075480fbd2a1e79f930
abushka110 <qyjodixilu@gmail.com>
2023-06-08 20:09:16 +0300

    task 7, third oop leson

---
+++ dot_net/task_8/task_8-1/task_8-1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public abstract class Planet
+{
+    public abstract void ReportAboutMovement();
+    public abstract void ReportAboutLife();
+}
+
+public class Earth : Planet
+{
+    public override void ReportAboutMovement()
+    {
+        Console.WriteLine("The Earth moves in an orbit around the Sun.");
+    }
+
+    public override void ReportAboutLife()
+    {
+        Console.WriteLine("Earth contains a variety of life.");
+    }
+}
+
+public class Moon : Planet
+{
+    public override void ReportAboutMovement()
+    {
+        Console.WriteLine("The moon moves in an orbit around the Earth.");
+    }
+
+    public override void ReportAboutLife()
+    {
+        Console.WriteLine("There is no evidence of life on the moon.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Planet[] planets = new Planet[2];
+
+        Earth earth = new Earth();
+        Moon moon = new Moon();
+
+        planets[0] = earth;
+        planets[1] = moon;
+
+        foreach (Planet planet in planets)
+        {
+            planet.ReportAboutMovement();
+            planet.ReportAboutLife();
+            Console.WriteLine();
+        }
+    }
+}
